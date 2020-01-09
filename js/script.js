d6bacd905465ea5384f16bb380ff8638a2acc8c7
Charles Kvoriak <s_charles.kvoriak@ousd.org>
2020-01-09 02:22:57 +0000

    Added title and div for songs

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let songs = [
+              ["Rhythm Section Want Ad", "They Might Be Giants", "https://youtu.be/GgDB2Q_R-Tk"],
+              ["Electioneering", "Radiohead"],
+              ["Colin Zeal", "Blur"]
+            ] 
